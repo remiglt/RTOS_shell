590bf9699da61f740bb38e2f7b756767849c5691
remiglt <94785697+remiglt@users.noreply.github.com>
2023-01-26 00:14:28 +0100

    Add files via upload

---
+++ Debug/shell/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../shell/shell.c 
+
+OBJS += \
+./shell/shell.o 
+
+C_DEPS += \
+./shell/shell.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+shell/%.o shell/%.su: ../shell/%.c shell/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F746xx -c -I../Core/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F7xx/Include -I../Drivers/CMSIS/Include -I"C:/Users/remig/STM32CubeIDE/workspace_1.10.1/rtos_td_shell/rtos_td_shell/shell" -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1 -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-shell
+
+clean-shell:
+	-$(RM) ./shell/shell.d ./shell/shell.o ./shell/shell.su
+
+.PHONY: clean-shell
+
